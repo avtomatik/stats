5ff7a2bcdc9e18d65973c6b0fe3bcc3ecd5891f3
Alexander <kkruscherr@gmail.com>
2023-04-29 12:37:16 +0300

    Extracted Constants

--- src/can/prices.py
+++ src/can/prices.py
@@ -1,35 +1,21 @@
 import pandas as pd
 
 from stats.src.can.combine import combine_can_price_a, combine_can_price_b
-from stats.src.can.constants import SERIES_IDS_CD, SERIES_IDS_PRCH
+from stats.src.can.constants import (SERIES_IDS_CD, SERIES_IDS_PRCH,
+                                     SERIES_IDS_PRICE_A, SERIES_IDS_PRICE_B)
 from thesis.src.lib.tools import construct_usa_hist_deflator
 
 df = pd.concat(
-    map(construct_usa_hist_deflator, (SERIES_IDS_CD, SERIES_IDS_PRCH)),
+    [
+        pd.concat(
+            map(construct_usa_hist_deflator, (SERIES_IDS_CD, SERIES_IDS_PRCH))
+        ),
+        combine_can_price_a(SERIES_IDS_PRICE_A),
+        combine_can_price_b(SERIES_IDS_PRICE_B),
+    ],
     axis=1,
     sort=True
 )
-
-CALLS = {
-    'cobb_douglas': construct_usa_hist_deflator(SERIES_IDS_CD),
-    'uscb': construct_usa_hist_deflator(SERIES_IDS_PRCH),
-    'canada_a': combine_can_price_a(),
-    'canada_b': combine_can_price_b(),
-}
-
-df = pd.concat(
-    [chunk for key, chunk in CALLS.items()],
-    axis=1
-)
-
-CALLS = (
-    construct_usa_hist_deflator(SERIES_IDS_CD),
-    construct_usa_hist_deflator(SERIES_IDS_PRCH),
-    combine_can_price_a,
-    combine_can_price_b,
-)
-
-df = pd.concat([call() for call in CALLS], axis=1)
 
 df['mean'] = df.mean(axis=1)
 df['cum_mean'] = df.iloc[:, -1].add(1).cumprod()
