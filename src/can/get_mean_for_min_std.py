89a2f444629dec6f22d261b436ef2fd9ae949197
Alexander <kkruscherr@gmail.com>
2023-04-30 15:12:21 +0300

    Updated `get_mean_for_min_std()`

--- src/can/get_mean_for_min_std.py
+++ src/can/get_mean_for_min_std.py
@@ -1,25 +1,35 @@
+import pandas as pd
+
+from stats.src.common.transform import transform_year_mean
 from thesis.src.lib.stockpile import stockpile_can
 
 
-def get_mean_for_min_std():
+def get_mean_for_min_std() -> tuple[int, float]:
     """
-    Determine Year & Mean Value for Base Vectors for Year with Minimum Standard Error
+    Determine Year & Mean Value for Base Series IDs for Year with Minimum Standard Error
+
+    Returns
+    -------
+    tuple[int, float]
+        DESCRIPTION.
+
     """
     FILE_NAME = 'stat_can_lab.csv'
     # =========================================================================
-    # Base Vectors
+    # Base Series IDs
     # =========================================================================
-    SERIES_IDS = {
-        'v123355112': 14100355,
-        'v1235071986': 14100392,
-        'v2057609': 14100355,
-        'v2057818': 14100355,
-        'v2523013': 14100027,
+    SERIES_IDS_NO_OPERATION = {'v2523013': 14100027, 'v1235071986': 14100392}
+    SERIES_IDS_TO_TRANSFORM = {
+        'v123355112': 14100355, 'v2057609': 14100355, 'v2057818': 14100355
     }
-
-    df = stockpile_can(SERIES_IDS).dropna(axis=0)
+    df = pd.concat(
+        [
+            stockpile_can(SERIES_IDS_NO_OPERATION),
+            stockpile_can(SERIES_IDS_TO_TRANSFORM).pipe(transform_year_mean)
+        ],
+        axis=1,
+        sort=True
+    ).dropna(axis=0)
     df['std'] = df.std(axis=1)
-    return (
-        df.iloc[:, [-1]].idxmin()[0],
-        df.loc[df.iloc[:, [-1]].idxmin()[0], :][:-1].mean()
-    )
+    year_min_std = int(df.iloc[:, [-1]].idxmin()[0])
+    return year_min_std, df.loc[year_min_std, :][:-1].mean()
